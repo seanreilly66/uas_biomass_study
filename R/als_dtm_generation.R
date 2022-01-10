427e5c9281a55d4607d042b73463ac8f50d8ae42
Sean Reilly <sean.reilly66@gmail.com>
2022-01-10 12:18:44 -0800

    Initial commit

--- R/als_dtm_generation.R
+++ R/als_dtm_generation.R
@@ -1,10 +1,67 @@
+# ==============================================================================
+#
+# LAS Ground Classification
+#
+# ==============================================================================
+#
+# Author: Sean Reilly, sean.reilly66@gmail.com
+#
+# Created: 10 Aug 2021
+# Last commit: 10 Aug 2021
+#
+# Status: Completed
+#
+# Created as part of 2021-2022 UAS biomass study.
+#
+# ==============================================================================
+#
+# Description:
+#
+# Generates DTM from ALS las files and converts the DTM to a LAS file format for
+# use in ground icp registration of UAS files.
+#
+# Setup for parallel batch processing of all las files in one parent folder
+#
+# ==============================================================================
+#
+# User inputs:
+#
+# las_folder = Folder containing las files with classified ground points
+# raster_output_folder = Location to which raster dtms should be exported
+# las_output_folder = Location to which las dtms should be exported
+#
+# ==============================================================================
+#
+# Package dependencies:
+#
+# lidr, tidyverse, glue, sf, doParallel
+#
+# ==============================================================================
+#
+# Known problems:
+#
+# ==============================================================================
+
 library(lidR)
 library(tidyverse)
 library(glue)
 library(sf)
 library(doParallel)
 
-las_files <- list.files('data/las/als', full.names = TRUE) %>%
+# ================================= User inputs ================================
+
+las_folder <- 'data/las/als'
+
+raster_output_folder <- 'data/dtm/als'
+
+las_output_folder <- 'data/las/icp_registration'
+
+
+# ==============================================================================
+# ============================ Generate DTM formats ============================ 
+# ==============================================================================
+
+las_files <- list.files(las_folder, full.names = TRUE) %>%
   str_subset('\\.las$')
 
 cl <- makeCluster(9)
@@ -15,13 +72,15 @@ foreach (
   .packages = c('lidR', 'tidyverse', 'glue', 'sf')
 ) %dopar% {
   
+  # --------------------------- Generate raster DTM ---------------------------- 
+  
   dtm <- readLAS(lf, select = 'c') %>%
     filter_ground() %>%
     grid_terrain(res = 0.5, algorithm = tin())
   
   dtm_name <- lf %>%
     str_replace('_als', '_als_dtm') %>%
-    str_replace('data/las/als', 'data/dtm/als')
+    str_replace('data/las/als', raster_output_folder)
   
   writeRaster(
     x = dtm,
@@ -29,6 +88,8 @@ foreach (
     datatype='FLT4S',
     format="GTiff",
     overwrite=TRUE)
+  
+  # ----------------------------- Generate LAS DTM -----------------------------
   
   dtm_las <- dtm %>%
     as.data.frame(xy = TRUE) %>%
@@ -44,10 +105,12 @@ foreach (
   
   dtm_las_name <- lf %>%
     str_replace('_als', '_als_dtm') %>%
-    str_replace('data/las/als', 'data/las/icp_registration')
+    str_replace('data/las/als', las_output_folder)
   
   writeLAS(dtm_las, dtm_las_name)
   
 }
   
-  
+stopCluster(cl)
+
+# ==============================================================================
