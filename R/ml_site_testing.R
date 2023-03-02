c5dd421a98a5aef6adb4d9bb4951379e41247892
Sean Reilly <sean.reilly66@gmail.com>
2023-03-01 17:06:09 -0800

    Update input data, remove non rf models

--- R/ml_site_testing.R
+++ R/ml_site_testing.R
@@ -51,90 +51,74 @@ library(glue)
 
 # ================================= User inputs ================================
 
-# Cluster file setup:
+# Input files
 
-# setwd('data/cluster_mirror')
-
-# response_csv <- 'data/plot_field_metrics.csv'
-# predictor_csv <- 'data/uas_plot_metrics.csv'
-
-# Local file setup: 
 response_csv <- 'data/field/plot_field_metrics.csv'
-predictor_csv <- 'data/las/metrics/uas_plot_metrics.csv'
+uas_csv <- 'data/las/metrics/uas_plot_metrics.csv'
+spec_csv <- 'data/las/metrics/spectral_plot_metrics.csv'
 
-output_file <-
-  'ml_site_testing_{type}_{format(timestamp, "%Y%m%d_%H%M")}'
+# Output
+output_file <- 'ml_site_testing_{type}_{format(timestamp, "%Y%m%d_%H%M")}'
 
+# Model training parameters
 
 k_folds <- 5
-rep <- 50
+rfe_rep <- 10
+training_rep <- 100
 
-set_seed_val = 111
+pre_process <- c('center', 'scale')
 
-n_core = 12
+set_seed_val <- 111
+
+n_cores <- detectCores() - 5
 
 # ==============================================================================
-# =========================== Model setup and inputs ===========================
+# ============================== Data preparation ==============================
 # ==============================================================================
+
+# Load inputs
 
 response_df <- read_csv(response_csv)
+
+uas_df <- read_csv(uas_csv)
+spec_df <- read_csv(spec_csv)
+
+predictor_df <- uas_df %>%
+  left_join(spec_df)
+
+# Extract variable names
 
 response_var <- response_df %>%
   select(-site, -campaign, -plot) %>%
   colnames() %>%
   str_subset('_n', negate = TRUE)
 
-
-predictor_df <- read_csv(predictor_csv)
-
 predictor_var <- predictor_df %>%
   select(-campaign, -plot, -method) %>%
   colnames()
 
-model_df <- response_df %>%
+n_predictors <- length(predictor_var)
+
+# Generate combined df for modelling
+
+model_df <- 
+  response_df %>%
   left_join(predictor_df)
 
 sites <- unique(model_df$site)
 
-rm(response_df, predictor_df)
-
-
-
+rm(response_df, predictor_df, uas_df, spec_df)
 
 # ==============================================================================
-# ========================== Response initialization ===========================
+# =============================== Log file setup ===============================
 # ==============================================================================
-
-# -------------------------------- Control setup -------------------------------
-
-train_ctrl <- trainControl(method = "repeatedcv",
-                           number = k_folds,
-                           repeats = rep)
-
-rfe_ctrl <- trainControl(method = "cv",
-                         number = k_folds)
-
-rf_ctrl <- rfeControl(
-  functions = rfFuncs,
-  method = "repeatedcv",
-  number = k_folds,
-  repeats = rep
-)
-
-svm_ctrl <- rfeControl(
-  functions = caretFuncs,
-  method = "repeatedcv",
-  number = k_folds,
-  repeats = rep
-)
-
-# ------------------------------- Log file setup -------------------------------
 
 timestamp <- Sys.time()
 
 log_text <- glue(
   '=====================================================================
-Site level model testing for canopy fuels prediction from UAS metrics
+Random Forest w/ RFE canopy fuels prediction from UAS metrics
+Individual site models
 =====================================================================
 
 author: Sean Reilly
@@ -143,16 +127,20 @@ initiated: {format(timestamp, "%Y-%m-%d %H:%M")}
 =============================== Inputs ==============================
 
 working directory: {getwd()}
-predictor df: {predictor_csv}
+predictor df: {uas_csv}, {spec_csv}
 response df: {response_csv}
 
-rf and svm preprocessing: center and scale
+N predictors: {length(predictor_var)}
 
-lm preprocessing: multicolinearity filter with R threshold = 0.9
+Preprocessing: center and scale
+
 
 ========================== Model parameters =========================
 
-repeats: {rep}
+random forest with RFE and spatial folds
+
+rfe repeats: {rfe_rep}
+training repeats: {training_rep}
 set seed: {set_seed_val}
 k folds: {k_folds}
 
@@ -163,19 +151,29 @@ k folds: {k_folds}
 '
 )
 
-# ------------------------------- Modelling run --------------------------------
 
-                # # # Testing setup
-                # log_text = log_text + '
-                #             #### ###### TESTING RUN ###### ####
-                # 
-                #             '
-                # response_var = response_var[1:2]
-                # sites = sites[1:2]
-                # predictor_var <- predictor_var[1:5]
-                # lm_predictor_var <- predictor_var[1:3]
+# ==============================================================================
+# ================================== Modelling ================================= 
+# ==============================================================================
 
+# ##### Testing setup #####
+# log_text = log_text + '
+# #### ###### TESTING RUN ###### ####
+# 
+# '
+# model_df <- model_df %>%
+#   add_column(foo = 1)
+# 
+# response_var = response_var[1:2]
+# predictor_var <- predictor_var[1:5] %>%
+#   append('foo')
+# n_predictors = length(predictor_var)
+# sites = sites[1:2]
+# 
+# response_i = response_var[1]
+# site_i = sites[1]
 
+# #####
 
 
 ml_rfe = list()
@@ -184,6 +182,7 @@ ml_results = list()
 ml_best = list()
 
 for (response_i in response_var) {
+  
   message('Response variable: ', response_i)
   
   log_text <- log_text + '
@@ -194,57 +193,49 @@ for (response_i in response_var) {
 '
   
   for (site_i in sites) {
+    
     message('Site: ', site_i)
-    
-    # ---------------------------- Dataframe setup -----------------------------
-    
-    input_df <- model_df %>%
-      filter(site == site_i) %>%
-      filter(!is.na(!!sym(response_i))) %>%
-      select(all_of(c(response_i, predictor_var))) %>%
-      as.data.frame()
-    
-    input_df[is.na(input_df)] <-  -9999
-    
-    cor_var <- input_df %>%
-      select(all_of(predictor_var)) %>%
-      cor(use = 'na.or.complete') %>%
-      findCorrelation(cutoff = 0.9,
-                      exact = TRUE,
-                      names = TRUE)
-    
-    lm_predictor_var <- predictor_var[!(predictor_var %in% cor_var)]
     
     log_text <- log_text + '
 ================================ {site_i} ================================= 
-rf and svm n predictors: {length(predictor_var)}
-lm forward predictors: {length(lm_predictor_var)}
 
 '
     
-    # ----------------------------------------------------------------------------
-    # -------------------------- Random Forest with RFE --------------------------
-    # ----------------------------------------------------------------------------
+    # ---------------------------- Model input setup -----------------------------
+    
+    input_df <- model_df %>%
+      filter(site == site_i) %>%
+      filter(!is.na(!!sym(response_i)))
+    
+    input_df[is.na(input_df)] <-  -9999
+    
+    ml_predictor <- input_df %>%
+      select(all_of(predictor_var))
+    
+    ml_response <- input_df %>%
+      pull(response_i)
     
     # ----------------------------------- RFE ------------------------------------
     
-    cl <- makeCluster(n_core)
+    cl <- makeCluster(n_cores)
     registerDoParallel(cl)
     
     set.seed(set_seed_val)
     
-    message('Random forest RFE')
+    message('RFE initiated ', Sys.time())
     
     ml_profile <- rfe(
-      x = input_df %>%
-        select(all_of(predictor_var)),
-      y = input_df %>%
-        pull(response_i),
-      sizes = c(2:length(predictor_var)),
-      rfeControl = rf_ctrl,
-      preProcess = c('center', 'scale'),
-      metric = "RMSE",
-      trControl = rfe_ctrl
+      x = ml_predictor,
+      y = ml_response,
+      sizes = c(1:n_predictors),
+      rfeControl = rfeControl(
+        functions = rfFuncs,
+        method = "repeatedcv",
+        number = k_folds,
+        repeats = rfe_rep
+      ),
+      preProcess = pre_process,
+      metric = "RMSE"
     )
     
     stopCluster(cl)
@@ -255,21 +246,22 @@ lm forward predictors: {length(lm_predictor_var)}
     
     # -------------------------------- Training ----------------------------------
     
-    cl <- makeCluster(n_core)
+    cl <- makeCluster(n_cores)
     registerDoParallel(cl)
     
     set.seed(set_seed_val)
     
-    message('Random forest w rfe model training')
+    message('Random forest initiated: ', Sys.time())
     
     ml_train <- train(
-      x = input_df %>%
+      x = ml_predictor %>%
         select(all_of(ml_var)),
-      y = input_df %>%
-        pull(response_i),
+      y = ml_response,
       method = "rf",
       preProcess = c('center', 'scale'),
-      trControl = train_ctrl,
+      trControl = trainControl(method = "repeatedcv",
+                               number = k_folds,
+                               repeats = training_rep),
       ntree = 1000,
       tuneLength = 100,
       metric = "RMSE"
@@ -301,8 +293,6 @@ lm forward predictors: {length(lm_predictor_var)}
     
     log_text <- log_text +
       '\n
-random forest with RFE
----------------------------------------------------------------------
 n samples: {nrow(input_df)}
 
 RFE:
@@ -316,292 +306,6 @@ MAE: {ml_stats$MAE}
 
 '
     
-#     # ----------------------------------------------------------------------------
-#     # --------------------------- Random Forest w/o RFE --------------------------
-#     # ----------------------------------------------------------------------------
-#     
-#     # -------------------------------- Training ----------------------------------
-#     
-#     cl <- makeCluster(n_core)
-#     registerDoParallel(cl)
-#     
-#     set.seed(set_seed_val)
-#     
-#     message('Random forest training')
-#     
-#     ml_train <- train(
-#       x = input_df %>%
-#         select(all_of(predictor_var)),
-#       y = input_df %>%
-#         pull(response_i),
-#       method = "rf",
-#       preProcess = c('center', 'scale'),
-#       trControl = train_ctrl,
-#       ntree = 1000,
-#       tuneLength = 100,
-#       metric = "RMSE"
-#     )
-#     
-#     stopCluster(cl)
-#     
-#     ml_models[[glue('{response_i}_{site_i}_rf')]] <- ml_train
-#     
-#     ml_results[[glue('{response_i}_{site_i}_rf')]] <-
-#       ml_train$results %>%
-#       add_column(
-#         response_var = response_i,
-#         site = site_i,
-#         method = 'rf',
-#         .before = 1
-#       )
-#     
-#     ml_stats <- ml_train$results %>%
-#       semi_join(ml_train$bestTune) %>%
-#       add_column(
-#         response_var = response_i,
-#         site = site_i,
-#         method = 'rf',
-#         .before = 1
-#       )
-#     
-#     ml_best[[glue('{response_i}_{site_i}_rf')]] <- ml_stats
-#     
-#     log_text <- log_text +
-#       '\n
-# random forest, no prior feature selection
-# ---------------------------------------------------------------------
-# 
-# n samples: {nrow(input_df)}
-# n variables: {length(predictor_var)}
-# 
-# Results:
-# 
-# RMSE: {ml_stats$RMSE}
-# R2: {ml_stats$Rsquared}
-# MAE: {ml_stats$MAE}
-# 
-# '
-    
-    # ----------------------------------------------------------------------------
-    # ------------------------------- SVM with RFE -------------------------------
-    # ----------------------------------------------------------------------------
-    
-    # ----------------------------------- RFE ------------------------------------
-    
-    cl <- makeCluster(n_core)
-    registerDoParallel(cl)
-    
-    set.seed(set_seed_val)
-    
-    message('SVM RFE')
-    
-    ml_profile <- rfe(
-      x = input_df %>%
-        select(all_of(predictor_var)),
-      y = input_df %>%
-        pull(response_i),
-      sizes = c(1:length(predictor_var)),
-      rfeControl = svm_ctrl,
-      method = 'svmRadial',
-      preProcess = c('center', 'scale'),
-      metric = "RMSE",
-      trControl = rfe_ctrl
-    )
-    
-    stopCluster(cl)
-    
-    ml_var <- predictors(ml_profile)
-    
-    ml_rfe[[glue('{response_i}_{site_i}_svm_rfe')]] <- ml_profile
-    
-    # -------------------------------- Training ----------------------------------
-    
-    cl <- makeCluster(n_core)
-    registerDoParallel(cl)
-    
-    set.seed(set_seed_val)
-    
-    message('svm w rfe model training')
-    
-    ml_train <- train(
-      x = input_df %>%
-        select(all_of(ml_var)),
-      y = input_df %>%
-        pull(response_i),
-      method = "svmRadial",
-      preProcess = c('center', 'scale'),
-      trControl = train_ctrl,
-      tuneLength = 100,
-      metric = "RMSE"
-    )
-    
-    stopCluster(cl)
-    
-    ml_models[[glue('{response_i}_{site_i}_svm_rfe')]] <- ml_train
-    
-    ml_results[[glue('{response_i}_{site_i}_svm_rfe')]] <-
-      ml_train$results %>%
-      add_column(
-        response_var = response_i,
-        site = site_i,
-        method = 'svm_rfe',
-        .before = 1
-      )
-    
-    ml_stats <- ml_train$results %>%
-      semi_join(ml_train$bestTune) %>%
-      add_column(
-        response_var = response_i,
-        site = site_i,
-        method = 'svm_rfe',
-        .before = 1
-      )
-    
-    ml_best[[glue('{response_i}_{site_i}_svm_rfe')]] <- ml_stats
-    
-    log_text <- log_text +
-      '\n
-SVM with RFE
----------------------------------------------------------------------
-n samples: {nrow(input_df)}
-
-RFE:
-n variables: {length(ml_var)}
-
-Results:
-
-RMSE: {ml_stats$RMSE}
-R2: {ml_stats$Rsquared}
-MAE: {ml_stats$MAE}
-
-'
-    
-#     # ----------------------------------------------------------------------------
-#     # -------------------------------- SVM w/o RFE -------------------------------
-#     # ----------------------------------------------------------------------------
-#     
-#     # -------------------------------- Training ----------------------------------
-#     
-#     cl <- makeCluster(n_core)
-#     registerDoParallel(cl)
-#     
-#     set.seed(set_seed_val)
-#     
-#     message('SVM training')
-#     
-#     ml_train <- train(
-#       x = input_df %>%
-#         select(all_of(predictor_var)),
-#       y = input_df %>%
-#         pull(response_i),
-#       method = "svmRadial",
-#       preProcess = c('center', 'scale'),
-#       trControl = train_ctrl,
-#       tuneLength = 100,
-#       metric = "RMSE"
-#     )
-#     
-#     stopCluster(cl)
-#     
-#     ml_models[[glue('{response_i}_{site_i}_svm')]] <- ml_train
-#     
-#     ml_results[[glue('{response_i}_{site_i}_svm')]] <-
-#       ml_train$results %>%
-#       add_column(
-#         response_var = response_i,
-#         site = site_i,
-#         method = 'svm',
-#         .before = 1
-#       )
-#     
-#     ml_stats <- ml_train$results %>%
-#       semi_join(ml_train$bestTune) %>%
-#       add_column(
-#         response_var = response_i,
-#         site = site_i,
-#         method = 'svm',
-#         .before = 1
-#       )
-#     
-#     ml_best[[glue('{response_i}_{site_i}_svm')]] <- ml_stats
-#     
-#     log_text <- log_text +
-#       '\n
-# SVM, no prior feature selection
-# ---------------------------------------------------------------------
-# 
-# n samples: {nrow(input_df)}
-# n variables: {length(predictor_var)}
-# 
-# Results:
-# 
-# RMSE: {ml_stats$RMSE}
-# R2: {ml_stats$Rsquared}
-# MAE: {ml_stats$MAE}
-# 
-# '
-    
-    # ----------------------------------------------------------------------------
-    # -------------------------------- LM w forward selection -------------------------------
-    # ----------------------------------------------------------------------------
-    
-    # -------------------------------- Training ----------------------------------
-    
-    message('leapForward')
-    
-    cl <- makeCluster(n_core)
-    registerDoParallel(cl)
-    
-    set.seed(set_seed_val)
-    
-    ml_train <-  train(
-      input_df %>%
-        select(all_of(lm_predictor_var)),
-      input_df %>%
-        pull(response_i),
-      method = 'leapForward',
-      preProcess = NULL,
-      trControl = train_ctrl
-    )
-    
-    stopCluster(cl)
-    
-    ml_models[[glue('{response_i}_{site_i}_leapForward')]] <-
-      ml_train
-    
-    ml_results[[glue('{response_i}_{site_i}_leapForward')]] <-
-      ml_train$results %>%
-      add_column(response_var = response_i,
-                 site = site_i,
-                 method = 'leapForward',
-                 .before = 1)
-    
-    ml_stats <- ml_train$results %>%
-      semi_join(ml_train$bestTune) %>%
-      add_column(response_var = response_i,
-                 site = site_i,
-                 method = 'leapForward',
-                 .before = 1)
-    
-    ml_best[[glue('{response_i}_{site_i}_leapForward')]] <- ml_stats
-    
-    log_text <- log_text +
-      '\n
-leapForward
----------------------------------------------------------------------
-
-n samples: {nrow(input_df)}
-
-Multicolinearity reduction:
-n variables: {length(lm_predictor_var)}
-
-Results:
-
-RMSE: {ml_stats$RMSE}
-R2: {ml_stats$Rsquared}
-MAE: {ml_stats$MAE}
-
-'
   }
 }
 
